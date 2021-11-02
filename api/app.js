a7a8055ea82ab6f9fe74a13aa6405c104c91a9ad
vovabatsyk <vovabatsyk.lviv@gmail.com>
2021-11-02 18:17:26 +0200

    Connect Backend & Frontend

--- api/app.js
+++ api/app.js
@@ -7,6 +7,11 @@ const bodyParser = require('body-parser')
 
 const {List, Task} = require('./db/models/index')
 
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*') // update to match the domain you will make the request from
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+    next()
+})
 
 app.use(bodyParser.json())
 
--- frontend/src/app/pages/task-view/task-view.component.ts
+++ frontend/src/app/pages/task-view/task-view.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core'
+import {TaskService} from '../../task.service'
 
 @Component({
   selector: 'app-task-view',
@@ -7,10 +8,16 @@ import {Component, OnInit} from '@angular/core'
 })
 export class TaskViewComponent implements OnInit {
 
-  constructor() {
+  constructor(private taskService:TaskService ) {
   }
 
   ngOnInit(): void {
+  }
+
+  createNewList() {
+    this.taskService.createList('New List').subscribe((response:any) => {
+      console.log(response)
+    })
   }
 
 }
