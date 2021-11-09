e06268223c9991d724e37a9749b5ec4cf707b405
vovabatsyk <vovabatsyk.lviv@gmail.com>
2021-11-09 23:03:23 +0200

    Add Login Page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import {RouterModule, Routes} from '@angular/router'
 import {TaskViewComponent} from './pages/task-view/task-view.component'
 import {NewListComponent} from './pages/new-list/new-list.component'
 import {NewTaskComponent} from './pages/new-task/new-task.component'
+import {LoginPageComponent} from './pages/login-page/login-page.component'
 
 const routes: Routes = [
   {path: '', redirectTo: 'lists', pathMatch: 'full'},
@@ -10,6 +11,7 @@ const routes: Routes = [
   {path: 'lists/:listId', component: TaskViewComponent},
   {path: 'lists', component: TaskViewComponent},
   {path: 'lists/:listId/new-task', component: NewTaskComponent},
+  {path: 'login', component: LoginPageComponent}
 ]
 
 @NgModule({
--- frontend/src/app/pages/new-task/new-task.component.ts
+++ frontend/src/app/pages/new-task/new-task.component.ts
@@ -1,17 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core'
 import {TaskService} from '../../task.service'
 import {ActivatedRoute, Params, Router} from '@angular/router'
 import {Task} from '../../models/task.model'
-import {THIS_EXPR} from '@angular/compiler/src/output/output_ast'
 
 @Component({
   selector: 'app-new-task',
   templateUrl: './new-task.component.html',
-  styleUrls: ['./new-task.component.scss']
+  styleUrls: ['./new-task.component.scss'],
 })
 export class NewTaskComponent implements OnInit {
 
-  constructor(private taskService:TaskService, private route: ActivatedRoute, private router: Router) { }
+  constructor(private taskService: TaskService, private route: ActivatedRoute, private router: Router) {
+  }
 
   // @ts-ignore
   listId: string
